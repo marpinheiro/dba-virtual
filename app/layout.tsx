7467e57bfd371ec880cd2dafe5248e1b5c7d11dd
Msilva <marpinheirosilva@gmail.com>
2025-12-28 12:13:48 -0300

    Adicionando Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// 1. Importar o Analytics
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CQLE DBA VIRTUAL", // Título da aba
-  description: "Seu assistente especialista em banco de dados",
-  icons: {
-    icon: '/cqle.ico', // Caminho do ícone que você colocou na pasta public
-  },
+  title: "CQLE DBA Virtual",
+  description: "Seu assistente de Banco de Dados com IA",
 };
 
 export default function RootLayout({
@@ -18,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        {children}
+        {/* 2. Adicionar o componente aqui */}
+        <Analytics />
+      </body>
     </html>
   );
 }
